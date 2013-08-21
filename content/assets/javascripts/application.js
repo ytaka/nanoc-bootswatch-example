f447171f9bfcb4bdf812d8f565839bbf4ece4912
Takayuki YAMAGUCHI <d@ytak.info>
2013-08-21 10:39:55 +0900

    Settings of bootswatch

--- content/assets/javascripts/application.js
+++ content/assets/javascripts/application.js
@@ -1,3 +1,4 @@
 //= require ../../../vendor/assets/stylesheets/bootstrap/assets/js/jquery.js
 //= require ../../../vendor/assets/stylesheets/bootstrap/dist/js/bootstrap.js
+//= require ../../../vendor/assets/stylesheets/bootswatch/assets/js/bootswatch.js
 //= require_tree .
