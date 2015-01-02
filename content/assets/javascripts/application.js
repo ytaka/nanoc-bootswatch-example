16eec753207214c3b40081ecfd0148aea08da7f8
Takayuki YAMAGUCHI <d@ytak.info>
2015-01-02 12:53:47 +0900

    Change to use nanoc-sprockets3

--- content/assets/javascripts/application.js
+++ content/assets/javascripts/application.js
@@ -1,5 +1,5 @@
-//= require ../../../vendor/assets/javascripts/jquery/test/data/jquery-1.9.1.js
-//= require ../../../vendor/assets/stylesheets/bootstrap/dist/js/bootstrap.js
-//= require ../../../vendor/assets/stylesheets/bootswatch/assets/js/bootswatch.js
-//= require ../../../vendor/assets/javascripts/jquery.ex-flex-fixed/jquery.exflexfixed-0.3.0.js
+//= require javascripts/jquery/test/data/jquery-1.9.1.js
+//= require stylesheets/bootstrap/dist/js/bootstrap.js
+//= require stylesheets/bootswatch/assets/js/bootswatch.js
+//= require javascripts/jquery.ex-flex-fixed/jquery.exflexfixed-0.3.0.js
 //= require_tree .
--- lib/sprockets.rb
+++ lib/sprockets.rb
@@ -1,16 +1,34 @@
 require "less"
 require "sass"
 require "uglifier"
-require "sprockets-helpers"
-require "sprockets-sass"
-require "sprockets-less"
-require "nanoc-sprockets-filter"
+require "nanoc-sprockets"
 require "nanoc-gzip-filter"
 
-include Nanoc::Helpers::Sprockets
+include Nanoc::Sprockets::Helper
 
-Nanoc::Helpers::Sprockets.configure do |config|
-  config.environment = Nanoc::Filters::Sprockets.environment
-  config.prefix      = "/assets"
-  config.digest      = false
+Nanoc::Sprockets::Helper.configure do |config|
+  config.environment = ::Sprockets::Environment.new(File.expand_path('.')) do |env|
+    # append defaults paths (if you want to)
+    Nanoc::Sprockets::Helper::DEFAULT_PATHS.each { |path| env.append_path path }
+    # append extra paths
+    env.append_path "content/assets"
+    env.append_path "vendor/assets"
+  end
+  config.prefix = '/assets'
+  config.digest = false
+end
+
+module Less
+  class Parser
+    class << self
+      alias :new_original :new
+    end
+
+    def self.new(options = {})
+      if options[:paths]
+        options[:paths] = options[:paths].dup
+      end
+      new_original(options)
+    end
+  end
 end
